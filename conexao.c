f192dc5c699d5c84827e45459739dc0a4133deed
Newton Eduardo Pena Villegas <newtonepv@gmail.com>
2024-09-18 16:13:57 -0300

    updated conexao

--- conexao.c
+++ conexao.c
@@ -1,21 +1,25 @@
 #include "conexao.h"
-#include "no.h"
 #include <stdlib.h>
     struct _conexao{
-        NO* no1;
-        NO* no2;
+        int* lsNos[2];
         int dist;
     };
 
-    CONEXAO* conexao_criar(int dist,NO*no1, NO* no2){
+    CONEXAO* conexao_criar(int dist,int no1, int no2){
         CONEXAO* conexao = (CONEXAO*)malloc(sizeof(CONEXAO));
         if(conexao){
-            conexao->no1 = no1;
-            conexao->no2 = no2;
+            conexao->lsNos[0] = no1;
+            conexao->lsNos[1] = no2;
             conexao->dist = dist;
             return conexao;
         }
         return NULL;
+    }
+    void conexao_apagar(CONEXAO** conexao){
+        if(*conexao){
+            free(*conexao);
+            *conexao = NULL;
+        }
     }
     int conexao_get_dist(CONEXAO* conexao){
         if(conexao){
@@ -23,15 +27,9 @@
         }
         return NULL;
     }
-    NO* conexao_get_no1(CONEXAO* conexao){
+    int conexao_get_no(CONEXAO* conexao, int index){
         if(conexao){
-            return conexao->no1;
-        }
-        return NULL;
-    }
-    NO* conexao_get_no2(CONEXAO* conexao){
-        if(conexao){
-            return conexao->no2;
+            return conexao->lsNos[index];
         }
         return NULL;
     }
--- conexao.h
+++ conexao.h
@@ -1,12 +1,11 @@
 #ifndef CONEXAO_H
     #define CONEXAO_H
     #include <stdio.h>
-    #include "no.h"
     typedef struct _conexao CONEXAO;
-    CONEXAO* conexao_criar(int dist, NO*no1, NO* no2);
+    CONEXAO* conexao_criar(int dist, int no1, int no2);
     int conexao_get_dist(CONEXAO* conexao);
-    NO* conexao_get_no1(CONEXAO* conexao);
-    NO* conexao_get_no2(CONEXAO* conexao);
-    
+    void conexao_apagar(CONEXAO** conexao);
+    int conexao_get_no(CONEXAO* conexao, int index);
+    void conexao_apagar(CONEXAO** conexao);
 
 #endif
